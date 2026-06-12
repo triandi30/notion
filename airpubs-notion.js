912f1d14d426d0e721eb6d05d8ebce087e52b993
triandi30 <52446442+triandi30@users.noreply.github.com>
2026-06-12 15:34:33 +0700

    Update airpubs-notion.js

--- airpubs-notion.js
+++ airpubs-notion.js
@@ -18,62 +18,59 @@
             var pagesDiv = article.querySelector('.meta .pages');
             var galleysList = article.querySelector('.galleys_links');
 
-            // Fetch article detail page
             fetch(articleUrl)
                 .then(function(res) { return res.text(); })
                 .then(function(html) {
                     var parser = new DOMParser();
                     var doc = parser.parseFromString(html, 'text/html');
 
-                    // Get authors with affiliations
-                    var authorItems = doc.querySelectorAll('.authors .list-group-item, .item.authors .sub_item');
+                    // Method 1: Get from meta tags (most reliable)
+                    var metaAuthors = doc.querySelectorAll('meta[name="citation_author"]');
+                    var metaInstitutions = doc.querySelectorAll('meta[name="citation_author_institution"]');
+
                     var names = [];
                     var affs = [];
 
-                    if (authorItems.length > 0) {
-                        authorItems.forEach(function(item, idx) {
-                            var nameEl = item.querySelector('.name') || item.querySelector('strong');
-                            var affEl = item.querySelector('.affiliation') || item.querySelector('.value:not(.name)');
-                            var name = nameEl ? nameEl.textContent.trim() : item.textContent.trim().split('\n')[0].trim();
-                            var aff = affEl ? affEl.textContent.trim() : '';
-                            if (name) {
-                                names.push(name);
-                                affs.push(aff);
+                    if (metaAuthors.length > 0) {
+                        metaAuthors.forEach(function(m, idx) {
+                            names.push(m.getAttribute('content'));
+                            // Get corresponding institution
+                            if (metaInstitutions[idx]) {
+                                affs.push(metaInstitutions[idx].getAttribute('content'));
+                            } else {
+                                affs.push('');
                             }
                         });
                     }
 
-                    // If no structured authors found, try alternate selectors
+                    // Fallback: Method 2 - from ul.authors li
                     if (names.length === 0) {
-                        var authBlock = doc.querySelector('.item.authors .value');
-                        if (authBlock) {
-                            var spans = authBlock.querySelectorAll('span.name, .author');
-                            spans.forEach(function(s) {
-                                names.push(s.textContent.trim());
-                            });
-                            var affSpans = authBlock.querySelectorAll('.affiliation');
-                            affSpans.forEach(function(s, i) {
-                                affs[i] = s.textContent.trim();
-                            });
-                        }
+                        var authorLis = doc.querySelectorAll('.item.authors ul.authors li, section.item.authors ul.authors li');
+                        authorLis.forEach(function(li) {
+                            var nameEl = li.querySelector('.name');
+                            var affEl = li.querySelector('.affiliation');
+                            if (nameEl) names.push(nameEl.textContent.trim());
+                            if (affEl) affs.push(affEl.textContent.trim());
+                            else affs.push('');
+                        });
                     }
 
                     // Build authors HTML with superscript
                     if (names.length > 0 && authorsDiv) {
                         var authHtml = '<div class="airpubs-authors"><i class="fas fa-users"></i> ';
                         names.forEach(function(name, idx) {
-                            authHtml += '<strong>' + name + '</strong> <sup>(' + (idx+1) + ')</sup>';
+                            authHtml += '<strong>' + name + '</strong> <sup>(' + (idx + 1) + ')</sup>';
                             if (idx < names.length - 1) authHtml += ', ';
                         });
                         authHtml += '</div>';
 
                         // Affiliations
-                        var hasAff = affs.some(function(a) { return a.length > 0; });
+                        var hasAff = affs.some(function(a) { return a && a.length > 0; });
                         if (hasAff) {
                             authHtml += '<div class="airpubs-affiliations">';
                             affs.forEach(function(aff, idx) {
                                 if (aff) {
-                                    authHtml += '<div class="airpubs-aff-line">(' + (idx+1) + ') ' + aff + '</div>';
+                                    authHtml += '<div class="airpubs-aff-line">(' + (idx + 1) + ') ' + aff + '</div>';
                                 }
                             });
                             authHtml += '</div>';
@@ -82,36 +79,23 @@
                         authorsDiv.innerHTML = authHtml;
                     }
 
-                    // Get abstract views
-                    var viewsEl = doc.querySelector('.item.views .value') || doc.querySelector('.views .value');
-                    var abstractViews = viewsEl ? viewsEl.textContent.trim() : '';
+                    // Get DOI from meta tag
+                    var doiMeta = doc.querySelector('meta[name="DC.Identifier.DOI"]');
+                    var doiText = doiMeta ? doiMeta.getAttribute('content') : '';
+                    var doiHref = doiText ? 'https://doi.org/' + doiText : '';
 
-                    // Get DOI
-                    var doiEl = doc.querySelector('.item.doi .value a') || doc.querySelector('.doi .value a');
-                    var doiText = '';
-                    var doiHref = '';
-                    if (doiEl) {
-                        doiHref = doiEl.getAttribute('href') || '';
-                        doiText = doiHref.replace('https://doi.org/', '').replace('http://doi.org/', '');
-                    }
+                    // Get pages from meta
+                    var pagesMeta = doc.querySelector('meta[name="DC.Identifier.pageNumber"]');
+                    var pages = pagesMeta ? pagesMeta.getAttribute('content') : '';
+                    if (!pages && pagesDiv) pages = pagesDiv.textContent.trim();
 
-                    // Get galley views
-                    var galleyViews = [];
-                    var galleyItems = doc.querySelectorAll('.item.galleys .value a, .galleys_links a');
-                    // Try to get download counts from article page
-                    var countEls = doc.querySelectorAll('.download .value, .item.galleys_count .value');
-
-                    // Build meta section
-                    var pages = pagesDiv ? pagesDiv.textContent.trim() : '';
+                    // Build enhanced meta section
                     var metaHtml = '<div class="airpubs-extra">';
 
                     // Stats row
                     metaHtml += '<div class="airpubs-meta-row">';
                     metaHtml += '<div class="airpubs-stats">';
-                    if (abstractViews) {
-                        metaHtml += '<span class="airpubs-stat"><i class="fas fa-chart-line"></i> Abstract : ' + abstractViews + '</span>';
-                    }
-                    // Galley labels
+                    // Galley download labels
                     if (galleysList) {
                         var gLinks = galleysList.querySelectorAll('a');
                         gLinks.forEach(function(g) {
@@ -145,15 +129,15 @@
                     if (galleysList) galleysList.style.display = 'none';
 
                     // Insert after meta div
-                    var metaDiv = article.querySelector('.meta');
-                    if (metaDiv) {
+                    var metaContainer = article.querySelector('.meta');
+                    if (metaContainer) {
                         var extraDiv = document.createElement('div');
                         extraDiv.innerHTML = metaHtml;
-                        metaDiv.parentNode.insertBefore(extraDiv, metaDiv.nextSibling);
+                        metaContainer.parentNode.insertBefore(extraDiv, metaContainer.nextSibling);
                     }
 
                 }).catch(function(err) {
-                    console.log('AiRPubs: Could not fetch article', err);
+                    console.log('AiRPubs: Error fetching', articleUrl, err);
                 });
         });
     });
